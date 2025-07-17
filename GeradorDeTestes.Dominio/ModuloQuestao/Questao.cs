5ffdcfb30bc3bc9c68451b9b1d690d5a024f8919
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-17 14:56:30 -0300

    feat: Implementa estrutura inicial do objeto Questao.

---
+++ GeradorDeTestes.Dominio/ModuloQuestao/Questao.cs
@@ -0,0 +1,26 @@
+﻿using GeradorDeTestes.Dominio.Compartilhado;
+using GeradorDeTestes.Dominio.ModuloMateria;
+
+namespace GeradorDeTestes.Dominio.ModuloQuestao;
+public class Questao : EntidadeBase<Questao>
+{
+    public Materia Materia { get; set; }
+    public string Enunciado { get; set; }
+    public List<Alternativa> Alternativas { get; set; } = new List<Alternativa>();
+
+    public Questao(Materia materia, string enunciado, List<Alternativa> alternativas)
+    {
+        Materia = materia;
+        Enunciado = enunciado;
+        foreach (Alternativa a in alternativas)
+        {
+            Alternativas.Add(a);
+        }
+    }
+
+    public override void AtualizarRegistro(Questao registroEditado)
+    {
+        Materia = registroEditado.Materia;
+        Enunciado = registroEditado.Enunciado;
+    }
+}
