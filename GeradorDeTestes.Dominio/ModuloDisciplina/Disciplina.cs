b2f09778e7b460f424e6e9886129012c7490f2e1
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-17 14:55:38 -0300

    feat: Implementa estrutura inicial do objeto Disciplina.

---
+++ GeradorDeTestes.Dominio/ModuloDisciplina/Disciplina.cs
@@ -0,0 +1,41 @@
+﻿using GeradorDeTestes.Dominio.Compartilhado;
+using GeradorDeTestes.Dominio.ModuloMateria;
+using GeradorDeTestes.Dominio.ModuloTeste;
+
+namespace GeradorDeTestes.Dominio.ModuloDisciplina;
+public class Disciplina : EntidadeBase<Disciplina>
+{
+    public string Nome { get; set; }
+    public List<Materia> Materias { get; set; } = new List<Materia>();
+    public List<Teste> Testes { get; set; } = new List<Teste>();
+
+    public Disciplina(string nome)
+    {
+        Nome = nome;
+    }
+
+    public void AderirMateria(Materia materia)
+    {
+        Materias.Add(materia);
+    }
+
+    public void RemoverMateria(Materia materia)
+    {
+        Materias.Remove(materia);
+    }
+
+    public void AderirTeste(Teste teste)
+    {
+        Testes.Add(teste);
+    }
+
+    public void RemoverTeste(Teste teste)
+    {
+        Testes.Remove(teste);
+    }
+
+    public override void AtualizarRegistro(Disciplina registroEditado)
+    {
+        Nome = registroEditado.Nome;
+    }
+}
