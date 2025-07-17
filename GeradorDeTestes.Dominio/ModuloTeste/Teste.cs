e6e201c839ecec4a6bd26f4bbfc31469ae36c3e5
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-17 14:56:36 -0300

    feat: Implementa estrutura inicial do objeto Teste.

---
+++ GeradorDeTestes.Dominio/ModuloTeste/Teste.cs
@@ -0,0 +1,46 @@
+﻿using GeradorDeTestes.Dominio.Compartilhado;
+using GeradorDeTestes.Dominio.ModuloDisciplina;
+using GeradorDeTestes.Dominio.ModuloMateria;
+using GeradorDeTestes.Dominio.ModuloQuestao;
+
+namespace GeradorDeTestes.Dominio.ModuloTeste;
+public class Teste : EntidadeBase<Teste>
+{
+    public string Titulo { get; set; }
+    public Disciplina Disciplina { get; set; }
+    public EnumSerie Serie { get; set; }
+    public Materia? Materia { get; set; }
+    public bool EhRecuperacao { get; set; }
+    public int QuantidadeQuestoes { get; set; }
+    public List<Questao> Questoes { get; set; } = new List<Questao>();
+
+    public Teste(string titulo, Disciplina disciplina, EnumSerie serie, Materia? materia, bool ehRecuperacao, int quantidadeQuestoes)
+    {
+        Titulo = titulo;
+        Disciplina = disciplina;
+        Serie = serie;
+        Materia = materia;
+        EhRecuperacao = ehRecuperacao;
+        QuantidadeQuestoes = quantidadeQuestoes;
+    }
+
+    public void AderirQuestao(Questao questao)
+    {
+        Questoes.Add(questao);
+    }
+
+    public void RemoverQuestao(Questao questao)
+    {
+        Questoes.Remove(questao);
+    }
+
+    public override void AtualizarRegistro(Teste registroEditado)
+    {
+        Titulo = registroEditado.Titulo;
+        Disciplina = registroEditado.Disciplina;
+        Serie = registroEditado.Serie;
+        Materia = registroEditado.Materia;
+        EhRecuperacao = registroEditado.EhRecuperacao;
+        QuantidadeQuestoes = registroEditado.QuantidadeQuestoes;
+    }
+}
