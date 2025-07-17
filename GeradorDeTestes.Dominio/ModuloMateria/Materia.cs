2de4ab46b8b438406b2d5e8af0b1f40af4dc0357
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-17 14:56:15 -0300

    feat: Implementa estrutura inicial dos objetos Materia e do enum EnumSerie.

---
+++ GeradorDeTestes.Dominio/ModuloMateria/Materia.cs
@@ -0,0 +1,24 @@
+﻿using GeradorDeTestes.Dominio.Compartilhado;
+using GeradorDeTestes.Dominio.ModuloDisciplina;
+
+namespace GeradorDeTestes.Dominio.ModuloMateria;
+public class Materia : EntidadeBase<Materia>
+{
+    public string Nome { get; set; }
+    public Disciplina Disciplina { get; set; }
+    public EnumSerie Serie { get; set; }
+
+    public Materia(string nome, Disciplina disciplina, EnumSerie serie)
+    {
+        Nome = nome;
+        Disciplina = disciplina;
+        Serie = serie;
+    }
+
+    public override void AtualizarRegistro(Materia registroEditado)
+    {
+        Nome = registroEditado.Nome;
+        Disciplina = registroEditado.Disciplina;
+        Serie = registroEditado.Serie;
+    }
+}
