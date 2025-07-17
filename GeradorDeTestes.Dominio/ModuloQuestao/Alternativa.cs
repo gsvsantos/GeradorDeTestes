9833ef70a41937c0efdd90b05bcb339408cea334
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-17 14:56:24 -0300

    feat: Implementa estrutura inicial do objeto Alternativa.

---
+++ GeradorDeTestes.Dominio/ModuloQuestao/Alternativa.cs
@@ -0,0 +1,16 @@
+﻿namespace GeradorDeTestes.Dominio.ModuloQuestao;
+
+public class Alternativa
+{
+    public Guid Id { get; set; }
+    public string Texto { get; set; }
+    public Questao Questao { get; set; }
+    public bool EstaCorreta { get; set; }
+
+    public Alternativa(string texto, Questao questao)
+    {
+        Id = Guid.NewGuid();
+        Texto = texto;
+        Questao = questao;
+    }
+}
