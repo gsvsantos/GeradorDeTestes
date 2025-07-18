a1192441cf530bf3f698d2dc8f796031ba3b22ee
RavoalM <alvarofeltrin2006@uniplaclages.edu.br>
2025-07-18 14:34:39 -0300

    refactor: Implementa DbSets para melhor geração de tabelas com E.F

--- GeradorDeTestes.Infraestrutura.ORM/Compartilhado/GeradorDeTestesDbContext.cs
+++ GeradorDeTestes.Infraestrutura.ORM/Compartilhado/GeradorDeTestesDbContext.cs
@@ -1,12 +1,16 @@
-﻿using GeradorDeTestes.Dominio.ModuloMateria;
+﻿using GeradorDeTestes.Dominio.ModuloDisciplina;
+using GeradorDeTestes.Dominio.ModuloMateria;
+using GeradorDeTestes.Dominio.ModuloQuestao;
 using GeradorDeTestes.Dominio.ModuloTeste;
 using Microsoft.EntityFrameworkCore;
 
 namespace GeradorDeTestes.Infraestrutura.ORM.Compartilhado;
 public class GeradorDeTestesDbContext : DbContext
 {
-    // Espaço para por as listas. Exemplo: public DbSet<Teste> Testes { get; set; }
+    public DbSet<Alternativa> Alternativas { get; set; }
+    public DbSet<Disciplina> Disciplinas { get; set; }
     public DbSet<Materia> Materias { get; set; }
+    public DbSet<Questao> Questoes { get; set; }
     public DbSet<Teste> Testes { get; set; }
 
     public GeradorDeTestesDbContext(DbContextOptions options) : base(options) { }
