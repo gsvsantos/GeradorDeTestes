db501575053ce0ca926e7423e91875224e97952f
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-18 15:49:39 -0300

    feat: Implementa QuestaoExtensions em WebApp.

---
+++ GeradorDeTestes.WebApp/Extensions/QuestaoExtensions.cs
@@ -0,0 +1,25 @@
+﻿using GeradorDeTestes.Dominio.ModuloMateria;
+using GeradorDeTestes.Dominio.ModuloQuestao;
+using GeradorDeTestes.WebApp.Models;
+
+namespace GeradorDeTestes.WebApp.Extensions;
+
+public static class QuestaoExtensions
+{
+    public static Questao ParaEntidade(this FormularioQuestaoViewModel formularioVM, Materia materia)
+    {
+        return new(
+            formularioVM.Enunciado,
+            materia);
+    }
+
+    public static DetalhesQuestaoViewModel ParaDetalhesVM(this Questao questao)
+    {
+        return new(
+            questao.Id,
+            questao.Enunciado,
+            questao.Materia.Nome,
+            questao.Alternativas,
+            questao.Testes);
+    }
+}
