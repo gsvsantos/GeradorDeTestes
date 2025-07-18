cccb1c32549c698c0d1bcc2183c50e6df949f6d1
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-18 15:47:45 -0300

    feat: Implementa QuestaoController com ação Index pronta, para visualização inicial das Questões, em WebApp.

---
+++ GeradorDeTestes.WebApp/Controllers/QuestaoController.cs
@@ -0,0 +1,30 @@
+﻿using GeradorDeTestes.Dominio.ModuloQuestao;
+using GeradorDeTestes.Infraestrutura.ORM.Compartilhado;
+using GeradorDeTestes.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeradorDeTestes.WebApp.Controllers;
+
+[Route("questoes")]
+public class QuestaoController : Controller
+{
+    private readonly GeradorDeTestesDbContext contexto;
+    //private readonly IRepositorioMateria repositorioMateria;
+    private readonly IRepositorioQuestao repositorioQuestao;
+
+    public QuestaoController(GeradorDeTestesDbContext contexto, /*IRepositorioMateria repositorioMateria,*/ IRepositorioQuestao repositorioQuestao)
+    {
+        this.contexto = contexto;
+        //this.repositorioMateria = repositorioMateria;
+        this.repositorioQuestao = repositorioQuestao;
+    }
+
+    public IActionResult Index()
+    {
+        List<Questao> questoes = repositorioQuestao.SelecionarRegistros();
+
+        VisualizarQuestoesViewModel visualizarVM = new(questoes);
+
+        return View(visualizarVM);
+    }
+}
