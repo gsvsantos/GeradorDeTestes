d9885ee1fbd337f4ace65519e58212a185ce8804
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-20 17:36:03 -0300

    feat: Implementa TesteExtensions em WebApp.

---
+++ GeradorDeTestes.WebApp/Extensions/TesteExtensions.cs
@@ -0,0 +1,29 @@
+﻿using GeradorDeTestes.Dominio.ModuloDisciplina;
+using GeradorDeTestes.Dominio.ModuloTeste;
+using GeradorDeTestes.WebApp.Models;
+
+namespace GeradorDeTestes.WebApp.Extensions;
+
+public static class TesteExtensions
+{
+    public static Teste ParaEntidade(this FormularioTesteViewModel formularioVM, Disciplina disciplina)
+    {
+        return new(
+            formularioVM.Titulo,
+            disciplina,
+            formularioVM.Serie,
+            formularioVM.EhProvao,
+            formularioVM.QuantidadeQuestoes);
+    }
+
+    public static DetalhesTesteViewModel ParaDetalhesVM(this Teste teste)
+    {
+        return new(
+            teste.Id,
+            teste.Titulo,
+            teste.Disciplina.Nome,
+            teste.Materias,
+            teste.EhProvao,
+            teste.QuantidadeQuestoes);
+    }
+}
