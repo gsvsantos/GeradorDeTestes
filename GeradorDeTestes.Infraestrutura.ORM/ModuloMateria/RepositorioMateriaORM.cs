efbbc4411a146ee65280db98d462cb0bae62f294
RavoalM <alvarofeltrin2006@uniplaclages.edu.br>
2025-07-19 21:30:56 -0300

    refactor: Implementa override de seleção de máteria

--- GeradorDeTestes.Infraestrutura.ORM/ModuloMateria/RepositorioMateriaORM.cs
+++ GeradorDeTestes.Infraestrutura.ORM/ModuloMateria/RepositorioMateriaORM.cs
@@ -1,9 +1,19 @@
 ﻿using GeradorDeTestes.Dominio.ModuloMateria;
 using GeradorDeTestes.Infraestrutura.ORM.Compartilhado;
+using Microsoft.EntityFrameworkCore;
 
 namespace GeradorDeTestes.Infraestrutura.ORM.ModuloMateria;
 
 public class RepositorioMateriaORM : RepositorioBaseORM<Materia>, IRepositorioMateria
 {
     public RepositorioMateriaORM(GeradorDeTestesDbContext contexto) : base(contexto) { }
+
+    public override List<Materia> SelecionarRegistros()
+    {
+        return registros.Include(mate => mate.Questoes).Include(mate => mate.Disciplina).ToList();
+    }
+    public override Materia? SelecionarRegistroPorId(Guid idRegistro)
+    {
+        return registros.Where(mate => mate.Id.Equals(idRegistro)).Include(mate => mate.Questoes).Include(mate => mate.Disciplina).FirstOrDefault();
+    }
 }
