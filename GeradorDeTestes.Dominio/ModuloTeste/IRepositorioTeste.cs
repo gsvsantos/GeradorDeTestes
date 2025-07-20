f6a08d12e11b68777698767a911d7b197e817901
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-20 17:26:27 -0300

    feat: Implementa interface de RepositorioTeste em Dominio.

---
+++ GeradorDeTestes.Dominio/ModuloTeste/IRepositorioTeste.cs
@@ -0,0 +1,8 @@
+﻿using GeradorDeTestes.Dominio.Compartilhado;
+using GeradorDeTestes.Dominio.ModuloMateria;
+
+namespace GeradorDeTestes.Dominio.ModuloTeste;
+public interface IRepositorioTeste : IRepositorio<Teste>
+{
+    public void AtualizarQuantidadePorMateria(Teste teste, Materia materia);
+}
