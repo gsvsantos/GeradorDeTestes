da52607b6d4cfe349c6f9b56e0982dcbb2d46264
gsvsantos <gustavocnsantos02@gmail.com>
2025-07-20 23:04:17 -0300

    refactor: Implementa extensões para GerarTesteViewModel e GerarProvaoViewModel.

--- GeradorDeTestes.WebApp/Extensions/TesteExtensions.cs
+++ GeradorDeTestes.WebApp/Extensions/TesteExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using GeradorDeTestes.Dominio.ModuloDisciplina;
+using GeradorDeTestes.Dominio.ModuloMateria;
 using GeradorDeTestes.Dominio.ModuloTeste;
 using GeradorDeTestes.WebApp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GeradorDeTestes.WebApp.Extensions;
 
@@ -25,5 +27,71 @@ public static class TesteExtensions
             teste.Materias,
             teste.EhProvao,
             teste.QuantidadeQuestoes);
+    }
+
+    public static GerarTesteViewModel ParaGerarTesteVM(this Teste teste, List<Materia> materias, List<Materia> materiasSelecionadas)
+    {
+        return new()
+        {
+            Id = teste.Id,
+            Titulo = teste.Titulo,
+            NomeDisciplina = teste.Disciplina.Nome,
+            Serie = teste.Serie,
+            QuantidadeQuestoes = teste.QuantidadeQuestoes,
+            Materias = materias.Select(m => new SelectListItem()
+            {
+                Text = m.Nome,
+                Value = m.Id.ToString()
+            }).ToList(),
+            MateriasSelecionadas = materiasSelecionadas.Select(m => new SelectListItem()
+            {
+                Text = m.Nome,
+                Value = m.Id.ToString()
+            }).ToList(),
+            QuantidadesPorMateria = teste.QuantidadesPorMateria.Select(qpm =>
+            new MateriaQuantidadeViewModel
+            {
+                MateriaId = qpm.Materia.Id,
+                QuantidadeQuestoes = qpm.QuantidadeQuestoes
+            }).ToList(),
+            Questoes = teste.Questoes.Select(q => new SelectListItem
+            {
+                Text = q.Enunciado,
+                Value = q.Id.ToString()
+            }).ToList()
+        };
+    }
+
+    public static GerarProvaoViewModel ParaGerarProvaoVM(this Teste teste, List<Materia> materias, List<Materia> materiasSelecionadas)
+    {
+        return new()
+        {
+            Id = teste.Id,
+            Titulo = teste.Titulo,
+            NomeDisciplina = teste.Disciplina.Nome,
+            Serie = teste.Serie,
+            QuantidadeQuestoes = teste.QuantidadeQuestoes,
+            Materias = materias.Select(m => new SelectListItem()
+            {
+                Text = m.Nome,
+                Value = m.Id.ToString()
+            }).ToList(),
+            MateriasSelecionadas = materiasSelecionadas.Select(m => new SelectListItem()
+            {
+                Text = m.Nome,
+                Value = m.Id.ToString()
+            }).ToList(),
+            QuantidadesPorMateria = teste.QuantidadesPorMateria.Select(qpm =>
+            new MateriaQuantidadeViewModel
+            {
+                MateriaId = qpm.Materia.Id,
+                QuantidadeQuestoes = qpm.QuantidadeQuestoes
+            }).ToList(),
+            Questoes = teste.Questoes.Select(q => new SelectListItem
+            {
+                Text = q.Enunciado,
+                Value = q.Id.ToString()
+            }).ToList()
+        };
     }
 }
